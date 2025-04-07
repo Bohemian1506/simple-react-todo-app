cb73d0ac0f7a2a2128470c2340bbd160ffeba454
Bohemian1506 <hiroshi151506@gmail.com>
2025-04-07 14:28:20 +0900

    Header.jsコンポーネントを追加

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>シンプルなTodoアプリ</h1>
+      <p>タスクを追加、完了、削除できます</p>
+    </header>
+  );
+}
+
+export default Header;
